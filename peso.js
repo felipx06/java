23c3a3453b2451df0942d784964519ce490c76fc
felipx06 <113368664+felipx06@users.noreply.github.com>
2023-10-04 08:38:28 -0300

    Add files via upload

--- peso.js
+++ peso.js
@@ -1,4 +1,11 @@
 const form = document.querySelector("form")
 const resp = document.querySelector("h3")
 
-form.addEventListene
+form.addEventListene("submit", (e) => {
+
+    const nome = form.inNome.value
+    const masculino = form.inMasculino.checked
+    const altura = Number(form.InAltura.value)
+
+    
+})
